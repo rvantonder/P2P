0e678d5974b37e8b553687a9fdae6ec814328080
Rai <rvantonder@gmail.com>
2011-10-04 07:56:25 -0500

    major server changes

---
+++ client/clientgui.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+import sip
+sip.setapi('QVariant', 2)
+import sys
+import select
+import threading
+import socket
+import pickle
+import os
+
+from PyQt4 import QtCore, QtGui
+from clientwindow import Ui_Form
+
+"""
+The Client GUI class.
+"""
+
+class ClientForm(QtGui.QWidget):
+  def __init__(self, host, port, usn):
+    super(ClientForm, self).__init__()
+    self.ui = Ui_Form()
+    self.ui.setupUi(self)
+    self.ui.lineEdit.setFocus()
+    self.icon = QtGui.QIcon('creeper.tif')
+    self.colour_list = (QtCore.Qt.red, QtCore.Qt.darkRed, QtCore.Qt.blue, QtCore.Qt.darkGreen, QtCore.Qt.magenta, QtCore.Qt.darkBlue, QtCore.Qt.darkCyan,QtCore.Qt.darkMagenta, QtCore.Qt.darkYellow, QtCore.Qt.darkGray, QtGui.QColor('#00CC99'), QtGui.QColor('#0099FF'), QtGui.QColor('#005555'), QtGui.QColor('#FF6600'), QtGui.QColor('#660033'), QtGui.QColor('#9900FF'))
+    self.user_colour_list = {}
+    self.running = 1
+
+    self.host = host
+    self.port = port
+    self.size = 1024
+    self.socket = None
+    self.username = ''
+
+    try:
+      self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.socket.connect((self.host, self.port))
+    except socket.error:
+      print 'Not accepting connections'
+      sys.exit(1)
+
+    try:
+      self.socket.send("\usn "+usn)
+      response = self.socket.recv(self.size)
+      if response == 'ACCEPT':
+        self.username = usn
+      else:
+        print 'Username already exists, please choose a different username'
+        sys.exit(1)
+    except socket.error:
+      print 'Some socket error'
+
+    self.receiver = Receiver(self.socket)
+    self.connect(self.receiver, QtCore.SIGNAL("update_msg"), self.update_msg)
+    self.connect(self.receiver, QtCore.SIGNAL("update_userlist"), self.update_userlist)
+    self.receiver.start() #start listening
+
+  def on_lineEdit_returnPressed(self):
+    if self.ui.lineEdit.displayText() != '':
+      stringToSend = self.ui.lineEdit.displayText()
+
+      try:
+        self.socket.send(str(stringToSend)) 
+      except socket.error:
+        self.ui.textEdit.setTextColor(QtCore.Qt.black)
+        self.ui.textEdit.setText('The connection with the server has been lost, please restart the client.')
+        self.ui.listWidget.clear() #delete the whole thing
+              
+    self.ui.lineEdit.setText('')
+
+  def update_userlist(self, l):
+    self.ui.listWidget.clear()
+
+    for i in l:
+      n = QtGui.QListWidgetItem(self.icon, str(i))
+      self.ui.listWidget.addItem(n)
+
+  def update_msg(self, msg):
+    self.ui.textEdit.append(msg)
+    self.ui.textEdit.ensurecursorVisible()
+
+class Receiver(QtCore.QThread):
+  def __init__(self, socket):
+    parent = None
+    QtCore.QThread.__init__(self, parent)
+    self.socket = socket
+    self.size = 1024
+    self.running = 1
+
+  def run(self):
+    while self.running: 
+      try:
+        response = self.socket.recv(self.size)
+        if not response:
+          self.socket.close()
+          self.emit(QtCore.SIGNAL("update_msg"), 'A connection error occurred, disconnected')
+        else:
+          display = self.parse_message(response)
+          if not display == None:
+            self.emit(QtCore.SIGNAL("update_msg"), display)
+      except socket.error:
+        print 'Unexpected error, disconnecting'
+        self.socket.close()
+        return
+
+    self.socket.close()
+
+  def parse_message(self, response):
+
+    if msg.startswith('(l'): #pickled userlist
+      userlist = pickle.loads(msg)
+
+      self.emit(QtCore.SIGNAL("update_userlist"), userlist.sort())
+      return None
+
+    self.emit(QtCore.SIGNAL("update_msg"), response)
+
+
+    """
+    cmd = ""
+    host = ""
+    msg = ""
+
+    try:
+      sdata = data.split(" ")
+    except:
+      pass
+
+    try:
+      cmd = sdata[0]
+    except:
+      print 'no cmd index'
+    
+    try:
+      host = sdata[1]
+      host.rstrip()
+    except:
+      print 'no host'
+       
+    try:
+      msg = data[len(cmd)+len(host):]
+    except:
+      print 'no msg'
+
+    if not cmd == r'\msg':
+    """
+      
+    
+
+if __name__ == '__main__':
+  gui = ClientForm('localhost',3001)
+
+  t = threading.Thread(target=gui.run)
+  t.setDaemon(True) #Sometimes programs spawn a thread as a daemon that runs without blocking the main program from exiting. Using daemon threads is useful for services where there may not be an easy way to interrupt the thread or where letting the thread die in the middle of its work does not lose or corrupt data
+  t.start()
+  gui.show()
+  sys.exit(app.exec_())
---
+++ server/server.py
@@ -0,0 +1,284 @@
+import select
+import socket
+import threading
+import sys
+from PyQt4 import QtCore, QtGui
+from serverwindow import Ui_Form
+
+global connections
+global calls #calls will be a list that contains n-tuples, each of which corresponds to a conference in progress
+
+class Client(QtCore.QThread):
+  def __init__(self,(client,address)):
+    QtCore.QThread.__init__(self,None) #parent = none
+    self.client = client #client, hostname?
+    self.address = address[0] 
+    self.port = str(address[1])
+    self.size = 1024
+    self.running = 1
+    self.username = ''
+
+  def run(self): #when the client thread is started
+
+
+    usn = self.client.recv(self.size)
+    if connections.has_key(usn):
+      self.client.send('REJECT')
+    else:
+      self.client.send('ACCEPT')
+
+    connections[usn] = self.client #username, socket pair
+    self.username = usn
+
+    self.emit(QtCore.SIGNAL("updateUserlist"), None)
+
+#    try:
+#      self.client.send("ul__ "+' '.join(connections.keys())+"\n")
+#      self.emit(QtCore.SIGNAL("updateText"), (self.address + " has connected"))
+#    except socket.error:
+#      print 'failed sending userlist'
+
+    while self.running:
+      try:
+        data = self.client.recv(self.size)
+      except socket.error as (number,msg):
+        del connections[self.address]
+        self.emit(QtCore.SIGNAL("updateUserlist"), None) #send data as test
+        self.emit(QtCore.SIGNAL("updateText"), (self.address + " has disconnected"))
+        return
+
+      if data:
+        cmd, host, msg = self.parse(data)
+        
+        if cmd == r'\call':
+          if self.address == host:
+            connections[self.address].send("You cannot call yourself")
+            break #TODO confirm working
+
+          self.emit(QtCore.SIGNAL("updateText"), (self.address + " wants to call " + host))
+          if host in connections.keys(): #TODO add callers to conferences
+            if self.host_in_call(host) or self.host_in_call(self.address): #check if either in call already
+              self.emit(QtCore.SIGNAL("updateText"), ("already in calls"))
+              connections[self.address].send("A call is already active, use \callc")
+              connections[host].send("A call is already active, use \callc")
+            else:
+              self.emit(QtCore.SIGNAL("updateText"), (host + " found"))
+              connections[self.address].send("ca__ "+host) #todo add call state variable
+              connections[host].send("ca__ "+self.address)
+              calls.append([self.address,host]) #append the call/conference
+              connections[self.address].send("You are now in a call with "+host)
+              connections[host].send("You are now in a call with "+self.address)
+            
+            #connect procedure
+          else:
+            self.emit(QtCore.SIGNAL("updateText"), (host + " not found"))
+            self.client.send("The host you wish to call does not exist")
+        elif cmd == r'\msg':
+          self.whisper(host, msg)
+        elif cmd == r'\dc':
+          print 'Current calls'
+          print calls
+          self.emit(QtCore.SIGNAL("updateText"), (self.address + " wants to disconnect a call "))
+
+          for conference in calls: 
+            if self.address in conference:
+              conference.remove(self.address) #remove myself from the call/conference
+              connections[self.address].send("dc__")
+              self.emit(QtCore.SIGNAL("updateText"), (self.address + " has been prompted to disconnect "))
+              if len(conference) == 1: #if there are less than two involved in the call
+                connections[conference[0]].send("dc__") #send the other host a dc__ command
+                self.emit(QtCore.SIGNAL("updateText"), (conference[0] + " has been prompted to disconnect "))
+                calls.remove(conference) #delete the entire call if there is only one active member 
+              elif len(conference) > 1:
+                for i in conference:
+                  connections[i].send(i+" has disconnected from the call")
+
+          print 'Removed calls'
+          print calls
+
+        elif cmd == r'\callc':
+          if self.host_in_call(host): #check if in call already
+            self.emit(QtCore.SIGNAL("updateText"), (self.address + "attempting conference call with " + host))
+            self.join_host_call(host)
+            channel_c = self.get_channel(host)
+            connections[self.address].send("You are now in a conference call with "+', '.join(channel_c)+"\n")
+            for h in channel_c:
+              connections[h].send("Adding "+self.address+" to the call")
+          else:
+            connections[self.address].send("There is no call active, you cannot make a conference call, use \call\n")
+
+          
+        else:
+          self.send_all(data)
+          self.emit(QtCore.SIGNAL("updateText"), (self.address + " sends msg " + data))
+   
+      else: #NO DATA
+        del connections[self.address]
+
+        for conference in calls: #TODO fix if already in call
+          if self.address in conference:
+            conference.remove(self.address) #remove myself from the call/conference
+            if len(conference) == 1: #if there are less than two involved in the call
+              connections[conference[0]].send("dc__") #send the other host a dc__ command
+              self.emit(QtCore.SIGNAL("updateText"), (conference[0] + " has been prompted to disconnect "))
+              calls.remove(conference) #delete the entire call if there is only one active member 
+            elif len(conference) > 1:
+              for i in conference:
+                connections[i].send(i+" has disconnected from the call")
+
+
+        print self.address,"removed from calls"
+        print "calls:"
+        print calls
+
+        self.emit(QtCore.SIGNAL("updateUserlist"), None) #send data as test
+        self.emit(QtCore.SIGNAL("updateText"), (self.address + " has disconnected"))
+        self.running = 0
+
+  def host_in_call(self, host):
+    for conference in calls:
+      if host in conference:
+        return True
+
+    return False
+
+  def get_channel(self, host):
+    for conference in calls:
+      if host in conference:
+        return conference
+
+  def join_host_call(self, host):
+    print "Adding",self.address,"to calls"
+    for conference in calls:
+      if host in conference:
+        conference.append(self.address)
+    print "calls:"
+    print calls
+
+  def send_all(self, msg):
+    for socket in connections.values():
+      try:
+        socket.send(self.address+': '+msg)
+      except IOError: 
+        print 'Socket already closed'
+
+  def whisper(self, host, msg):
+    if connections.has_key(host):
+      connections[host].send("whisper from "+self.address+":"+msg)
+      self.client.send("whisper to "+self.address+":"+msg)
+    else:
+      self.client.send("Sorry you cannot whisper to "+host+" because they do not exist")
+
+  def parse(self, data):
+    
+    cmd = "" 
+    host = ""
+    msg = ""
+    try:
+      sdata = data.split(" ")
+    except:
+      pass
+
+    try:
+      cmd = sdata[0]
+    except:
+      print 'No cmd index'
+    
+    try:
+      host = sdata[1]
+      host.rstrip()
+    except:
+      print 'No host'
+  
+    try:
+      msg = data[len(cmd)+len(host):]
+    except:
+      print 'No msg'
+
+    if not cmd == r'\call' and not cmd == r'\msg':
+      if cmd.startswith(r'\\'):
+        self.emit(QtCore.SIGNAL("updateText"), ("command " + cmd + " from " + self.address + " not valid"))
+
+    return cmd, host, msg
+       
+class ServerGUI(QtGui.QWidget):
+  def __init__(self,port):
+
+    super(ServerGUI, self).__init__()
+
+    self.ui = Ui_Form()
+    self.ui.setupUi(self)
+    
+    self.host = ''
+    self.port = port
+    self.backlog = 5
+    self.size = 1024
+    self.socket = None
+    self.threads = []
+
+    try:
+      self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.socket.bind((self.host,self.port))
+      self.socket.listen(self.backlog)
+    except socket.error:
+      print "Could not open port. Aborting."
+      sys.exit(1)
+
+    self.c = None
+
+  def run(self):
+    input = [self.socket,sys.stdin]
+    running = 1
+
+    while running:
+        inputready,outputready,exceptready = select.select(input,[],[])
+
+        for s in inputready: #the polling loop, between sockets and stdin
+
+            if s == self.socket:
+                c = Client(self.socket.accept())
+                self.connect(c, QtCore.SIGNAL("updateUserlist"), self.updateUserlist)
+                self.connect(c, QtCore.SIGNAL("updateText"), self.updateText)
+                c.start()
+                self.threads.append(c)
+
+            elif s == sys.stdin:
+                junk = sys.stdin.readline()
+                running = 0
+
+  def updateUserlist(self):
+    self.ui.listWidget.clear()
+
+    for i in connections.keys(): #update my userlist
+      item = QtGui.QListWidgetItem(str(i))
+      self.ui.listWidget.addItem(item) 
+
+    userlist = pickle.dumps(connection.keys()) 
+
+    for socket in connections.values(): #send the updated userlist
+      try:
+        socket.send(userlist)
+      except IOError: 
+        print 'Socket already closed'
+    
+  def updateText(self, msg):
+    self.ui.textEdit.append(msg)
+    self.ui.textEdit.ensureCursorVisible()
+
+
+if __name__ == '__main__':
+
+  connections = {}
+  calls = []
+
+  try:
+    app = QtGui.QApplication(sys.argv)
+    gui = ServerGUI(int(sys.argv[1]))
+    t = threading.Thread(target=gui.run)
+    t.setDaemon(True)
+    t.start()
+    gui.show()
+    sys.exit(app.exec_())
+  except IndexError:
+    print 'Usage: python voip_server.py <port>'
+
